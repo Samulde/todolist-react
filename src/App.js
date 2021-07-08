09743defd570cb5ac5715954708f13e0fa30ac57
samulde <samulde@gmail.com>
2021-07-08 15:29:54 +1000

    Add completed, deleted functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 // Importing Components
@@ -6,14 +6,17 @@ import Form from './Components/Form';
 import TodoList from './Components/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([])
+  
   return (
     <div className="App">
       <header>
         <h1>Samulde's To-Do list</h1>
       </header>
-      <Form />
-      <TodoList />
-      
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
+      <TodoList todos={todos} setTodos={setTodos} />
+
     </div>
   );
 }
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+//Import Components
+import Todo from './Todo'
 
-const TodoList = () => {
+
+const TodoList = ({ todos, setTodos }) => {
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                {todos.map(todo => (
+                    <Todo 
+                        key={todo.id} 
+                        text={todo.text} 
+                        todo={todo}
+                        todos={todos}
+                        setTodos={setTodos}
+                    />
+                ))}
+            </ul>
         </div>
     );
 }
